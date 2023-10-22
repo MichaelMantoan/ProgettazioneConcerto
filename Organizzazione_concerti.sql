da519a01feefaf297cbb9cb7f150426cbdfdde06
MichaelMantoan <98811594+MichaelMantoan@users.noreply.github.com>
2023-10-22 18:51:46 +0200

    Delete Organizzazione_concerti.sql

--- Organizzazione_concerti.sql
+++
@@ -1,9 +0,0 @@
-create database Organizzazione_Concerto;
-create table Organizzazione_Concerto.concerti(
-    id int not null auto_increment primary key,
-    codice varchar(50),
-    titolo varchar(50),
-    descrizione varchar(100),
-    data datetime
-
-);
